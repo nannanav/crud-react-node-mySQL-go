da947ce3206cb67e48522647b96e1263073f5c5e
Nannan <nannannadhan@gmail.com>
2025-03-05 14:53:10 -0800

    env var fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+// export default ({ mode }) => {
+//   const env = loadEnv(mode, process.cwd());
 
-  return defineConfig({
-    plugins: [react()],
-    define: {
-      'import.meta.env.VITE_BACKEND_URL': JSON.stringify(env.VITE_BACKEND_URL),
-    },
-  });
-}
+//   return defineConfig({
+//     plugins: [react()],
+//     define: {
+//       'import.meta.env.VITE_BACKEND_URL': JSON.stringify(process.env.VITE_BACKEND_URL),
+//       'import.meta.env.VITE_BACKEND_URL': JSON.stringify(env.VITE_BACKEND_URL),
+//       'import.meta.env.VITE_BACKEND_URL': JSON.stringify(
+//         process.env.VITE_BACKEND_URL || env.VITE_BACKEND_URL
+//       ),
+//     },
+//   });
+// }
+
+export default defineConfig({
+  plugins: [react()],
+})
--- smoke_test.sh
+++ smoke_test.sh
@@ -2,12 +2,13 @@
 set -e
 # Run Docker Compose with build
 docker-compose -f docker-compose-local.yml up --build -d
-
+echo "Docker Compose ran successfully."
+exit 0
 # Check if the command succeeded
-if [ $? -eq 0 ]; then
-    echo "Docker Compose ran successfully."
-    exit 0
-else
-    echo "Docker Compose failed."
-    exit 1
-fi
+# if [ $? -eq 0 ]; then
+#     echo "Docker Compose ran successfully."
+#     exit 0
+# else
+#     echo "Docker Compose failed."
+#     exit 1
+# fi
